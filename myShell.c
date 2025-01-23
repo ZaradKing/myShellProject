cb44824ad207028d5230b644def81c918b89c02e
macbook M1 <a1111@Zarads-MacBook-Air.local>
2025-01-23 13:36:13 +0200

    Added welcome message

---
+++ myShell.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void showWelcomeMessage() {
+    printf("aSPY//YASa\n");
+    printf("apyyyyCY//////////YCa       | Welcome to myShell\n");
+    printf("ayyyyyyySCP//Pp            | Version 2.4.3\n");
+    printf("pCCCCY//p          cSSps y//Y   | https://github.com/<user>\n");
+    printf("Have fun!\n");
+    printf("------------------------------------------------\n");
+    printf("Craft packets like it is your last day on earth.\n");
+}
+
+int main() {
+    showWelcomeMessage();
+    return 0;
+}
